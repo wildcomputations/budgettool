70d4c2d3cf1dd92f84e2e4f24ddde72c204cbd0a
Emilie Phillips <emilie@j3.org>
2016-03-27 20:32:11 -0400

    Changed schedule interface to have explicit view.
    Some schedules have built in start and end.
    All schedules have a view start and end

--- budgettool/schedulers.py
+++ budgettool/schedulers.py
@@ -5,72 +5,124 @@ Dates are returned as instances of datetime.date
 import datetime
 import math
 
+def _check_calc_date_range(default_start,
+        user_start,
+        default_end,
+        user_end,
+        user_duration):
+    """ Helper function to compute an end date from a start and an end date or
+    duration which may be none.
+    """
+    if user_start is None:
+        start = default_start
+    else:
+        start = user_start
+
+    if start is None:
+        raise ValueError("No start specified")
+    if default_start is not None:
+        start = max(default_start, start)
+
+    if user_end is not None:
+        end = user_end
+    elif user_duration is not None:
+        end = user_start + user_duration
+    else:
+        end = default_end
+
+    if end is None:
+        raise ValueError("No end specified")
+
+    if default_end is not None:
+        end = min(end, default_end)
+
+    return start, end
+
 class Once:
     """ One time transaction. Generates a single event on the specified date.
     """
-    def __init__(self, date, iter_start=datetime.date.today()):
+    def __init__(self, date):
         self.date = date
-        self.iter_start = iter_start
 
-    def __iter__(self):
-        if self.iter_start <= self.date:
-            return [self.date].__iter__()
+    def view(self, start=datetime.date.today(), end=None, duration=None):
+        start, end = _check_calc_date_range(
+                self.date, start,
+                self.date + datetime.timedelta(1), end, duration)
+        if start <= self.date and self.date < end:
+            return [self.date]
         else:
-            return [].__iter__()
+            return []
 
 class _FixedIncrIter:
     """Fixed increment in days
     """
-    def __init__(self, next_date, end_date, increment):
+    def __init__(self, container):
         """Increment up to, but excluding end_date
         """
-        self.next_date = next_date
-        self.end_date = end_date
-        self.increment = increment
+        self.container = container
+        self.next_date = container.start_date
     def __iter__(self):
         return self
     def __next__(self):
-        if self.next_date >= self.end_date:
+        if self.next_date >= self.container.end_date:
             raise StopIteration
         out = self.next_date
-        self.next_date += self.increment
+        self.next_date += self.container.increment
         return out
+
+class _FixedIncrContainer:
+    """Fixed increment in days
+    """
+    def __init__(self, start_date, end_date, increment):
+        """Increment up to, but excluding end_date
+        """
+        self.start_date = start_date
+        self.end_date = end_date
+        self.increment = increment
+    def __iter__(self):
+        return _FixedIncrIter(self)
 
 class EveryNWeek:
     """ Repeating schedule on weekly increments.
     For example, repeat every 2 weeks starting on Jan 1.
     """
-    def __init__(self, start, end, step=1, iter_start=datetime.date.today()):
+    def __init__(self, start, step=1, end=None):
         """Create the schedule. By default it will repeat every week and won't
         generate old dates before today.
 
         Params
         -----
         start - datetime.date object for the first instance of this event
-        end - cuttoff for last event to generate
         step - the number of weeks between events
-        iter_start - cutoff for earliest event to generate.
+        end - cuttoff for last event to generate
         """
         self.start = start
-        self.step = step * datetime.timedelta(days=7)
+        self.step = step
         self.end = end
-        self.iter_start = iter_start
 
-    def __iter__(self):
-        if self.start < self.iter_start:
-            delta = self.iter_start - self.start
-            num_steps = math.ceil(delta / self.step)
-            next_date = self.start + num_steps * self.step
+    def view(self, start=datetime.date.today(), end=None, duration=None):
+        iter_start, iter_end = _check_calc_date_range(
+                self.start, start,
+                self.end, end, duration)
+        if iter_start >= iter_end:
+            return []
+
+        step = self.step * datetime.timedelta(days=7)
+
+        if self.start < iter_start:
+            delta = iter_start - self.start
+            num_steps = math.ceil(delta / step)
+            next_date = self.start + num_steps * step
         else:
             next_date = self.start
 
-        return _FixedIncrIter(next_date, self.end, self.step)
+        return _FixedIncrContainer(next_date, iter_end, step)
 
 class Weekly:
     """ Repeat every week on a specifc day of the week.
     For example, repeat every Tuesday.
     """
-    def __init__(self, day_of_week, end, iter_start=datetime.date.today()):
+    def __init__(self, day_of_week, start=None, end=None):
         """ Repeat every week on the specified day of the week.
         Defaults to only generating events in the future.
 
@@ -81,10 +133,19 @@ class Weekly:
         end - cuttoff for last event to generate
         iter_start: cuttoff for earliest event to generate.
         """
-        offset = (day_of_week - iter_start.weekday()) % 7
-        self.start = iter_start + datetime.timedelta(offset)
+        self.day_of_week = day_of_week
+        self.start = start
         self.end = end
 
-    def __iter__(self):
-        return _FixedIncrIter(self.start, self.end, datetime.timedelta(days=7))
+    def view(self, start=datetime.date.today(), end=None, duration=None):
+        iter_start, iter_end = _check_calc_date_range(
+                self.start, start,
+                self.end, end, duration)
+        if iter_start >= iter_end:
+            return []
+
+        offset = (self.day_of_week - iter_start.weekday()) % 7
+        iter_start = iter_start + datetime.timedelta(offset)
+
+        return _FixedIncrContainer(iter_start, iter_end, datetime.timedelta(days=7))
 
--- budgettool/tests/test_schedulers.py
+++ budgettool/tests/test_schedulers.py
@@ -8,66 +8,93 @@ import unittest
 
 from .. import schedulers
 
-class _TestSchedulers(unittest.TestCase):
+class TestSchedulers(unittest.TestCase):
     def test_once(self):
         """Test Once schedule
         """
         earlier = date(2015, 10, 3)
         later = date(2016, 2, 15)
 
-        sched = schedulers.Once(later, earlier)
-        out = list(sched)
+        sched = schedulers.Once(later)
+        out = list(sched.view(start=earlier))
         self.assertEqual(out, [later])
 
-        sched = schedulers.Once(earlier, later)
-        out = list(sched)
+        sched = schedulers.Once(earlier)
+        out = list(sched.view(later))
         self.assertEqual(out, [])
+
+        sched = schedulers.Once(date(2016, 1, 6))
+        out = list(sched.view(later, earlier))
+        self.assertEqual(out, [])
+
+        sched = schedulers.Once(earlier)
+        out = list(sched.view(start=earlier, duration=timedelta(5)))
+        self.assertEqual(out, [earlier])
+
+        sched = schedulers.Once(earlier)
+        out = list(sched.view(start=earlier, duration=timedelta(0)))
+        self.assertEqual(out, [])
+
+        sched = schedulers.Once(later)
+        out = list(sched.view(start=earlier))
+        self.assertEqual(out, [later])
 
         today = date.today()
         tomorrow = date.today() + timedelta(1)
         yesterday = date.today() - timedelta(1)
 
         sched = schedulers.Once(tomorrow)
-        out = list(sched)
+        out = list(sched.view())
         self.assertEqual(out, [tomorrow])
 
         sched = schedulers.Once(today)
-        out = list(sched)
+        out = list(sched.view())
         self.assertEqual(out, [today])
 
         sched = schedulers.Once(yesterday)
-        out = list(sched)
+        out = list(sched.view())
         self.assertEqual(out, [])
 
     def test_every_n_week(self):
         """Test events that repeat on multi-week interval
         """
         TestItem = namedtuple("TestItem",
-                ['start', 'end', 'step', 'iter_start'])
+                ['start', 'end', 'step', 'iter_start', 'iter_end'])
         # Test cases must generate at least one event for the subsequent loop
         # to run
         test_items = [
                 TestItem(date(2011, 5, 20), date(2012, 1, 1),
-                    None, date(2011, 5, 20)),
+                    None, date(2011, 5, 20), None),
                 TestItem(date(2012, 3, 1), date(2040, 12, 31),
-                    52, date(2012, 3, 1)),
+                    52, date(2012, 3, 1), None),
                 TestItem(date(2011, 5, 20), date(2012, 1, 1),
-                    3, date(2011, 1, 1)),
+                    3, date(2011, 1, 1), None),
                 TestItem(date(2010, 3, 8), date(2012, 1, 1),
-                    4, date(2011, 5, 20)),
+                    4, date(2011, 5, 20), None),
+                TestItem(date(2011, 5, 20), None,
+                    None, date(2011, 5, 20), date(2012, 1, 1)),
+                TestItem(date(2012, 3, 1), None,
+                    52, date(2012, 3, 1), date(2040, 12, 31)),
+                TestItem(date(2011, 5, 20), None,
+                    3, date(2011, 1, 1), date(2012, 1, 1)),
+                TestItem(date(2010, 3, 8), None,
+                    4, date(2011, 5, 20), date(2012, 1, 1)),
+                TestItem(date(2010, 3, 3), date(2011, 12, 18),
+                    5, date(2011, 5, 20), date(2012, 1, 1)),
                 ]
 
         for test in test_items:
             if test.step is None:
-                sched = schedulers.EveryNWeek(
-                        test.start, test.end, iter_start=test.iter_start)
                 days_per_step = 7
+                sched = schedulers.EveryNWeek(test.start, end=test.end)
             else:
-                sched = schedulers.EveryNWeek(
-                        test.start, test.end, test.step, iter_start=test.iter_start)
                 days_per_step = 7 * test.step
+                sched = schedulers.EveryNWeek(test.start, test.step, test.end)
 
-            out = list(sched)
+            if test.iter_end is None:
+                out = list(sched.view(start=test.iter_start))
+            else:
+                out = list(sched.view(start=test.iter_start, end=test.iter_end))
             self.assertGreaterEqual(out[0], test.start)
             self.assertGreaterEqual(out[0], test.iter_start)
             delta_to_start = out[0] - test.start
@@ -76,33 +103,43 @@ class _TestSchedulers(unittest.TestCase):
             prev = out[0]
             for event in out[1:]:
                 self.assertEqual((event - prev).days, days_per_step)
-                self.assertLess(event, test.end)
+                if test.end is not None:
+                    self.assertLess(event, test.end)
+                if test.iter_end is not None:
+                    self.assertLess(event, test.iter_end)
                 prev = event
+
+        # test durations
+        starts = [date(2011, 1, 2),
+                date(2011, 3, 4),
+                date(2011, 5, 6),
+                date(2011, 7, 8)
+                ]
+        for start in starts:
+            sched = schedulers.EveryNWeek(start)
+
+            out = list(sched.view(sched.start, duration=timedelta(7 * 5)))
+            self.assertEqual(len(out), 5)
 
         # test special cases that the general loop above can't handle
         # end == start
-        sched = schedulers.EveryNWeek(
-                date(2015, 3, 2), date(2015, 3, 2), iter_start=date(2015, 1, 1))
-        out = list(sched)
+        sched = schedulers.EveryNWeek(date(2015, 3, 2))
+        out = list(sched.view(start=date(2015, 1, 1), end=date(2015, 3, 2)))
         self.assertEqual(out, [])
 
         # end < start
-        sched = schedulers.EveryNWeek(
-                date(2015, 10, 30), date(2015, 8, 1), iter_start=date(2015, 1, 1))
-        out = list(sched)
+        sched = schedulers.EveryNWeek(date(2015, 10, 30))
+        out = list(sched.view(end=date(2015, 8, 1), start=date(2015, 1, 1)))
         self.assertEqual(out, [])
 
         # iter_start < end
-        sched = schedulers.EveryNWeek(
-                date(2015, 3, 2), date(2015, 10, 30), iter_start=date(2016, 1, 1))
-        out = list(sched)
+        sched = schedulers.EveryNWeek(date(2015, 3, 2))
+        out = list(sched.view(end=date(2015, 10, 30), start=date(2016, 1, 1)))
         self.assertEqual(out, [])
 
         # test default value for iter_start. Should be today
-        sched = schedulers.EveryNWeek(
-                date.today() - timedelta(7 * 100 + 1),
-                date.today() + timedelta(365))
-        event = next(iter(sched))
+        sched = schedulers.EveryNWeek(date.today() - timedelta(7 * 100 + 1))
+        event = next(iter(sched.view(end=date.today() + timedelta(365))))
         self.assertEqual(event, date.today() + timedelta(6))
 
     def test_weekly(self):
@@ -129,16 +166,49 @@ class _TestSchedulers(unittest.TestCase):
                 }
 
         for day_id, first_date in weekdays.items():
-            sched = schedulers.Weekly(day_id, end, start)
-            out = list(sched)
+            sched = schedulers.Weekly(day_id)
+            out = list(sched.view(end=end, start=start))
             self.assertEqual(len(out), num)
             for x in range(num):
                 self.assertEqual(out[x], first_date + x * timedelta(7))
 
         tomorrow = date.today() + timedelta(1)
-        sched = schedulers.Weekly(tomorrow.weekday(), tomorrow + timedelta(7))
-        out = list(sched)
+        sched = schedulers.Weekly(tomorrow.weekday())
+        out = list(sched.view(end=tomorrow + timedelta(7)))
         self.assertEqual(out, [tomorrow])
+
+        TestRanges = namedtuple("TestRanges",
+                ["sched_start", "sched_end", "view_start", "view_end"])
+        test_cases = (
+                # view inside schedule
+                TestRanges(date(2015, 1, 15), date(2015, 6, 15),
+                    date(2015, 2, 15), date(2015, 5, 15)),
+                # view greater than schedule
+                TestRanges(date(2016, 3, 1), date(2016, 5, 1),
+                    date(2016, 1, 1), date(2016, 6, 1)),
+                # view before & overlapping
+                TestRanges(date(2015, 8, 20), date(2017, 4, 23),
+                    date(2014, 9, 30), date(2015, 12, 28)),
+                # view after and overlapping
+                TestRanges(date(2015, 8, 20), date(2017, 4, 23),
+                    date(2016, 9, 30), date(2017, 12, 28)),
+                # no intersection
+                TestRanges(date(2015, 1, 15), date(2015, 6, 15),
+                    date(2016, 9, 30), date(2017, 4, 23))
+                )
+        for test in test_cases:
+            sched = schedulers.Weekly(2, test.sched_start, test.sched_end)
+            view_with_end = sched.view(test.view_start, test.view_end)
+            for event in view_with_end:
+                self.assertEqual(event.weekday(), 2)
+                self.assertGreaterEqual(event, test.sched_start)
+                self.assertGreaterEqual(event, test.view_start)
+                self.assertLess(event, test.sched_end)
+                self.assertLess(event, test.view_end)
+            
+            view_with_duration = sched.view(test.view_start,
+                    duration = test.view_end - test.view_start)
+            self.assertEqual(list(view_with_end), list(view_with_duration))
 
 if __name__ == '__main__':
     unittest.main()
