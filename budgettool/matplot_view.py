d0457acf6262143c3d8ae87d2e5f4778b76ddf81
Emilie Phillips <emilie@j3.org>
2016-04-07 22:47:35 -0400

    now made matplotlib import optional

--- budgettool/matplot_view.py
+++ budgettool/matplot_view.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
 def plot_forecast(forecast):
+    import matplotlib.pyplot as plt
+    import numpy as np
     x_values = np.array([entry.date for entry in forecast])
     y_values = np.array([entry.balance for entry in forecast])
     plt.plot(x_values, y_values)
