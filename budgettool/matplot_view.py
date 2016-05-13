daf1741a3293a5f603bde381b295d7204f0192d9
Emilie Phillips <emilie@j3.org>
2016-05-12 22:07:41 -0400

    more readable date axis

--- budgettool/matplot_view.py
+++ budgettool/matplot_view.py
@@ -4,8 +4,11 @@ def plot_forecast(forecast):
     """Plot a graph of the forecast."""
     import matplotlib.pyplot as plt
     import numpy as np
+    
     x_values = np.array([entry.date for entry in forecast])
     y_values = np.array([entry.balance for entry in forecast])
+    figure = plt.figure()
     plt.plot(x_values, y_values)
+    figure.autofmt_xdate()
     plt.grid()
     plt.show()
