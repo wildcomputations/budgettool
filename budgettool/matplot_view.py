7df74ca571e6a1a72b08bd7ba6a25d33175393fe
Emilie Phillips <emilie@j3.org>
2016-06-08 21:47:05 -0400

    removed premature optimization to not import plotting tools.

--- budgettool/matplot_view.py
+++ budgettool/matplot_view.py
@@ -1,9 +1,9 @@
 """Plot graph of forecast."""
+import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_forecast(forecast):
     """Plot a graph of the forecast."""
-    import matplotlib.pyplot as plt
-    import numpy as np
     
     x_values = np.array([entry.date for entry in forecast])
     y_values = np.array([entry.balance for entry in forecast])
